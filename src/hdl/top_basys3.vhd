8ae6d4f16a4928f323e6af52fa0eb1e5b50c51c1
joel-bryant7 <141947808+joel-bryant7@users.noreply.github.com>
2024-04-16 17:57:57 -0600

    basic functionality completed

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -92,21 +92,77 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
   
 	-- declare components and signals
-
-  
+    component elevator_controller_fsm is
+        Port ( i_clk     : in  STD_LOGIC;
+               i_reset   : in  STD_LOGIC;
+               i_stop    : in  STD_LOGIC;
+               i_up_down : in  STD_LOGIC;
+               o_floor   : out STD_LOGIC_VECTOR (3 downto 0)           
+             );
+    end component elevator_controller_fsm;
+     
+    component sevenSegDecoder is
+              Port ( 
+                i_D : in STD_LOGIC_VECTOR (3 downto 0);
+                o_S : out STD_LOGIC_VECTOR (6 downto 0)
+              );
+    end component sevenSegDecoder;
+    
+    component clock_divider is
+              generic ( constant k_DIV : natural := 2);
+              port (    
+                 i_clk    : in std_logic;           -- basys3 clk
+                 i_reset  : in std_logic;           -- asynchronous
+                 o_clk    : out std_logic           -- divided (slow) clock
+                );
+            end component clock_divider; 
+            
+     signal w_clk : std_logic;
+     signal w_floor : std_logic_vector (3 downto 0);
+     signal w_Ereset : std_logic;
+     signal w_Creset : std_logic;
+     
 begin
 	-- PORT MAPS ----------------------------------------
-
+    elevator_controller_fsm_inst : elevator_controller_fsm
+        port map(
+            i_clk     => w_clk,
+            i_reset   => w_Ereset,
+            i_up_down => sw(1),
+	        i_stop    => sw(0),
+	        o_floor   => w_floor
+	    );
 	
+	sevenSegDecoder_inst : sevenSegDecoder
+	   port map(
+	       i_D => w_floor,
+	       o_S => seg
+	  );
+	  
+	clock_divider_inst : clock_divider
+	   generic map ( k_DIV => 25000000 ) -- 2 Hz clock from 100 MH
+	   port map(
+	       i_reset => w_Creset,
+	       i_clk => clk,
+	       o_clk => w_clk 
+	   );
 	
 	-- CONCURRENT STATEMENTS ----------------------------
+	-- w logic etc
+	w_Ereset <= btnR or btnU;
+	w_Creset <= btnL or btnU;
 	
 	-- LED 15 gets the FSM slow clock signal. The rest are grounded.
-	
+	led(15) <= w_clk;
+	led(14 downto 0) <= (others => '0');
 
 	-- leave unused switches UNCONNECTED. Ignore any warnings this causes.
 	
 	-- wire up active-low 7SD anodes (an) as required
 	-- Tie any unused anodes to power ('1') to keep them off
+	an(3) <= '1';
+	an(2) <= '0';
+	an(1) <= '1';
+	an(0) <= '1';
 	
 end top_basys3_arch;
